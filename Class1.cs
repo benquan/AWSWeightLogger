067bda1fae3a3ecf791414ae98fb45efe0336624
Ben Quan <benquan@gmail.com>
2017-04-18 21:39:43 -0600

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    namespace GenericHid
+    {
+        class WSUser
+        {
+            public string Name;
+            public int age;
+            public DateTime birthdate;
+            public int ID;
+        }
+
+
+        public class WSRegistry
+        {
+            public int userID;
+            public DateTime fecha;
+            public float weightKG;
+            public float boneKG;
+            public float fat;
+            public float water;
+            public float Muscle;
+
+            public void clear()
+            {
+                //fecha = null;
+                weightKG = 0;
+                boneKG = 0;
+                fat = 0;
+                water = 0;
+                Muscle = 0;
+            }
+        }
+
+
+
+        public class reader
+        {
+            private WSUser currentUser;
+            private int currentProcess;
+            private int currentPart;
+            private int currentRegistry;
+            private int currentRegistryFound;
+        }
+    }
